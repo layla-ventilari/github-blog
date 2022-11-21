86cfc672b1354f1852d94d02c561ba23da4aeeaf
Layla Ventillari <laylaventilari5@gmail.com>
2022-11-21 17:20:24 -0300

    post

--- src/pages/Blog/components/Profile/index.tsx
+++ src/pages/Blog/components/Profile/index.tsx
@@ -1,7 +1,7 @@
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { ExternalLink } from "../../../../components/ExternalLink";
 import { ProfileContainer, ProfileDetails, ProfilePicture } from "./styles";
-
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 export function Profile(){
   return(
     <ProfileContainer>
@@ -9,8 +9,23 @@ export function Profile(){
       <ProfileDetails>
         <header>
           <h1>Layla Ventilari</h1>
-        <ExternalLink text="Github" href="#"/>
+          <ExternalLink text="Github" href="#"/>
         </header>
+        <p>
+        Tristique volutpat pulvinar vel massa, pellentesque egestas.
+        Eu viverra massa quam dignissim aenean malesuada suscipit. 
+        Nunc, volutpat pulvinar vel mass.
+        </p>
+        <ul>
+          <li>
+            <FontAwesomeIcon icon={faGithub} />
+              LaylaVentillari
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faLinkedin}/>
+            layla-ventilari-135574145
+          </li>
+        </ul>
       </ProfileDetails>
     </ProfileContainer> 
   )

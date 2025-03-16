28bba10cb735561fb30c4591427d4d0dfe949ee0
Layla Ventilari <99200113+layla-ventilari@users.noreply.github.com>
2025-03-16 18:41:33 +0000

    styles: apply light theme

--- src/pages/Blog/components/Profile/styles.ts
+++ src/pages/Blog/components/Profile/styles.ts
@@ -1,60 +1,61 @@
 import styled from "styled-components";
 import headerBg from "../../../../assets/bg-light.svg";
 
+export const ProfileContainer = styled.section`
+  width: 100%;
+  min-height: 13.25rem;
+  margin-top: -5.5rem;
+  background: url(${headerBg}) no-repeat center;
+  background-size: cover;
+  box-shadow: 0px 2px 28px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  padding: 2rem 2.5rem;
+  display: flex;
+  gap: 2rem;
+`;
 
-export const ProfileContainer = styled.section `
-width: 100%;
-min-height: 13.25rem;
-margin-top: -5.5rem;
-background: url(${headerBg}) no-repeat center; 
-background-size: cover;
-box-shadow: 0px 2px 28px rgba(0,0,0,0.2);
-border-radius: 10px;
-padding: 2rem 2.5rem;
-display: flex;
-gap: 2rem;
-
-`
 export const ProfilePicture = styled.img`
-width: 9.25rem;
-height: 9.25rem;
-border-radius: 8px;
-object-fit: cover;
+  width: 9.25rem;
+  height: 9.25rem;
+  border-radius: 8px;
+  object-fit: cover;
 `;
 
 export const ProfileDetails = styled.div`
-flex: 1;
-display: flex;
-flex-direction: column;
-
-header {
+  flex: 1;
   display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-bottom: 0.5rem;
+  flex-direction: column;
 
-h1 {
-  font-size: ${({ theme }) => theme.textSizes["title-title-l"]};
-  color: ${({ theme }) => theme.colors["blue-100"]};
-  line-height: 130%;
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 0.5rem;
+
+    h1 {
+      font-size: ${({ theme }) => theme.textSizes["title-title-l"]};
+      color: ${({ theme }) => theme.colors["blue-100"]};
+      line-height: 130%;
+    }
   }
-}
-ul {
-  display: flex;
-  align-items: center;
-  gap: 1.5rem;
-  flex-wrap: wrap;
-  margin-top: auto;
-}
-li{
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-}
-svg{
-  width: 1.125rem;
-  height: 1.125rem;
-  color: ${({ theme}) => theme.colors["base-label"]};
-}
 
-`
+  ul {
+    display: flex;
+    align-items: center;
+    gap: 1.5rem;
+    flex-wrap: wrap;
+    margin-top: auto;
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  svg {
+    width: 1.125rem;
+    height: 1.125rem;
+    color: ${({ theme }) => theme.colors["base-label"]};
+  }
+`;
